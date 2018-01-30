3fc4cb1ab1d8d9a6910f9010f6aad46aaaa9fbdd
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-30 17:28:26 +0100

    fixed errors in heartbeat.RunFileHeartbeat and NewConfigFromEnv

--- heartbeat/heartbeat.go
+++ heartbeat/heartbeat.go
@@ -1,67 +1,68 @@
 package heartbeat
 
 import (
-  "fmt"
-  "os"
-  "strconv"
-  "time"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
 )
 
 const (
-  FileKey     = "HEARTBEAT_FILE"
-  IntervalKey = "HEARTBEAT_INTERVAL"
+	FileKey     = "HEARTBEAT_FILE"
+	IntervalKey = "HEARTBEAT_INTERVAL"
 )
 
 // RunFileHeartbeat loops heartbeat to file emition in
 // an infinite loop, with a specified interval.
 func RunFileHeartbeat(filepath string, sleepSeconds int) {
-  for {
-    EmitToFile(filepath)
-    time.Sleep(sleepSeconds * time.Second)
-  }
+	for {
+		EmitToFile(filepath)
+		time.Sleep(time.Duration(sleepSeconds) * time.Second)
+	}
 }
 
 // EmitToFile Changes the time modified on a specified file,
 // creates the file if not present.
 func EmitToFile(filepath string) {
-  now := getNow()
-  err := os.Chtimes(filepath, now, now)
-  if err != nil {
-    log.Println(err)
-    createFile(filepath)
-  }
+	now := getNow()
+	err := os.Chtimes(filepath, now, now)
+	if err != nil {
+		log.Println(err)
+		createFile(filepath)
+	}
 }
 
 // Config configuration for running file heartbeat.
 type Config struct {
-  File     string `json:"file"`
-  Interval int    `json:"interval"`
+	File     string `json:"file"`
+	Interval int    `json:"interval"`
 }
 
 // NewConfigFromEnv reads heartbeat configuration from environment variables
 func NewConfigFromEnv() (Config, error) {
-  config := Config{File: os.Getenv(FileKey)}
-  if config.File == "" {
-    return config, fmt.Error("Heartbeat Config.File not set")
-  }
-  interval, err := strconv.Atoi(os.Getenv(IntervalKey))
-  if err != nil {
-    return config, err
-  }
-  config.Interval = interval
-  return config, nil
+	config := Config{File: os.Getenv(FileKey)}
+	if config.File == "" {
+		return config, fmt.Errorf("Heartbeat Config.File not set")
+	}
+	interval, err := strconv.Atoi(os.Getenv(IntervalKey))
+	if err != nil {
+		return config, err
+	}
+	config.Interval = interval
+	return config, nil
 }
 
 // createFile creates a new file named as specified by the filepath.
 func createFile(filepath string) {
-  f, err := os.Create(filepath)
-  if err != nil {
-    log.Fatal(err)
-  }
-  f.Close()
+	f, err := os.Create(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Close()
 }
 
 // getNow gets the current UTC timestamp.
 func getNow() time.Time {
-  return time.Now()
+	return time.Now()
 }
